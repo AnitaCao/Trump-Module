67d8bac6613a822a1422a57b58722ec98ea5de63
AnitaCao <tingcao@abdn.ac.uk>
2014-07-22 16:18:21 +0100

    test

--- omod/src/main/java/org/openmrs/module/trumpmodule/web/resource/PatientAssignmentsResource.java
+++ omod/src/main/java/org/openmrs/module/trumpmodule/web/resource/PatientAssignmentsResource.java
@@ -1,20 +1,13 @@
 package org.openmrs.module.trumpmodule.web.resource;
 
-
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import luca.tmac.basic.data.uris.ProvenanceStrings;
-
-import org.openmrs.OpenmrsData;
-import org.openmrs.Patient;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.trumpmodule.OpenmrsEnforceServiceContext;
 import org.openmrs.module.trumpmodule.patientassignment.PatientAssignment;
-import org.openmrs.module.trumpmodule.policies.Policy;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
@@ -35,14 +28,21 @@ import com.hp.hpl.jena.tdb.TDBFactory;
 
 
 @Resource(name ="v1/trumpmodule/patientassignment", supportedClass = PatientAssignment.class, supportedOpenmrsVersions = {"1.8.*", "1.9.*"})
-public class PatientAssignmentResource extends DataDelegatingCrudResource<PatientAssignment> {
+public class PatientAssignmentsResource extends DataDelegatingCrudResource<PatientAssignment> {
 
 
+	
+	
+	
+	
+	private Dataset dataset;
+	OpenmrsEnforceServiceContext openmrsContext = OpenmrsEnforceServiceContext.getInstance();
+	private String directory = openmrsContext.getProvenanceDirectory();
+	
 	@Override
 	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
 		if (rep instanceof DefaultRepresentation) {
 			DelegatingResourceDescription description = new DelegatingResourceDescription();
-			//description.addProperty("patientName");
 			description.addProperty("patientUUID");
 			description.addProperty("doctorId");
 			description.addSelfLink();
@@ -66,9 +66,7 @@ public class PatientAssignmentResource extends DataDelegatingCrudResource<Patien
 	public DelegatingResourceDescription getCreatableProperties() {
 		DelegatingResourceDescription description = new DelegatingResourceDescription();
 		description.addRequiredProperty("doctorId");
-		//description.addRequiredProperty("patientName");
 		description.addRequiredProperty("patientUUID");
-		//description.addRequiredProperty("userId");
 		return description;
 	}
 
@@ -76,9 +74,7 @@ public class PatientAssignmentResource extends DataDelegatingCrudResource<Patien
 	public DelegatingResourceDescription getUpdatableProperties() {
 		DelegatingResourceDescription description = new DelegatingResourceDescription();
 		description.addRequiredProperty("doctorId");
-		//description.addRequiredProperty("patientName");
 		description.addRequiredProperty("patientUUID");
-		//description.addRequiredProperty("userId");
 
 		return description;
 	}
@@ -97,10 +93,60 @@ public class PatientAssignmentResource extends DataDelegatingCrudResource<Patien
 		//String patientName = patient.getGivenName()+" "+patient.getMiddleName()+" " +patient.getFamilyName();
 		
 		//delegate.setPatientName(patientName);
+		long startTime = System.currentTimeMillis();
+		dataset = TDBFactory.createDataset(directory);
+		ProvenanceBundle provBundle = new ProvenanceBundle(ProvenanceStrings.NS);
 		
-		delegate.setPatientName("dave"); //just for test
+		//insert to TDB
+		String activityURI = provBundle.createActivity();
+		com.hp.hpl.jena.rdf.model.Resource activity = provBundle.getResource(activityURI);
 		
+		// add statement describing when the activity started
+		provBundle.addStartedAtTime(activity, startTime);
+		// agent - comes from the logged in user or the user who is invoking the method
+		User user = Context.getAuthenticatedUser();
+		String agentURI = provBundle.createAgent(ProvenanceStrings.NS
+				+ ProvenanceStrings.AGENT + user.getId());
+		com.hp.hpl.jena.rdf.model.Resource agent = provBundle.getResource(agentURI);
+		
+		// the activity was started by the agent. 
+		provBundle.addWasStartedBy(activity, agent); 
+		
+		Property actionProp = provBundle.getModel().createProperty( ProvenanceStrings.NS, ProvenanceStrings.ACTIVITY_NS);
+		
+		activity.addProperty(actionProp, "do_patient_assignment");
+		
+		String entityURI = provBundle.createEntity(ProvenanceStrings.NS
+				+ ProvenanceStrings.ENTITY_PATIENT_ASSIGNMENT
+				+ delegate.getUuid());
+		
+		com.hp.hpl.jena.rdf.model.Resource entity = provBundle.getResource(entityURI);
+		
+		Property entityProp = provBundle.getModel().createProperty(ProvenanceStrings.NS, ProvenanceStrings.PATIENT_NAME);
+		
+		entity.addProperty(entityProp, delegate.getPatientName());
+		
+		String agentURI1 = provBundle.createAgent(ProvenanceStrings.NS + ProvenanceStrings.AGENT + delegate.getDoctorId());
+		
+		com.hp.hpl.jena.rdf.model.Resource agent1 = provBundle.getResource(agentURI1);
+		
+		String agentURI2 = provBundle.createAgent(ProvenanceStrings.NS + ProvenanceStrings.AGENT + delegate.getPatientUUID());
+		
+		com.hp.hpl.jena.rdf.model.Resource agent2 = provBundle.getResource(agentURI2);
+		
+		provBundle.addWasAssignedTo(agent1, agent2);
+		
+		// add statement describing when the activity ended.
+		provBundle.addEndedAtTime(activity, System.currentTimeMillis());
 
+		provBundle.getModel().write(System.out);
+
+		Model model = dataset.getDefaultModel();
+
+		model.add(provBundle.getModel());
+
+		dataset.close();
+		
 		return delegate;
 
 	}
@@ -109,14 +155,12 @@ public class PatientAssignmentResource extends DataDelegatingCrudResource<Patien
 	protected void delete(PatientAssignment delegate, String reason,
 			RequestContext context) throws ResponseException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void purge(PatientAssignment delegate, RequestContext context)
 			throws ResponseException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -126,6 +170,7 @@ public class PatientAssignmentResource extends DataDelegatingCrudResource<Patien
 
 	@Override
 	public PatientAssignment getByUniqueId(String uniqueId) {
+		
 		//TODO need to change here to getting by uuid
 		return new PatientAssignment();
 	}
