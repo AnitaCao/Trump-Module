54885645f03a0cc6d1ffa41c3b31dad35dacabbc
AnitaCao <tingcao@abdn.ac.uk>
2014-10-29 17:05:35 +0000

    Added all the concepts from the questionnaire, which is 16 questions (5 concepts for each question)

--- concepts/concepts.py
+++ concepts/concepts.py
@@ -8,29 +8,38 @@ headers = {'content-type': 'application/json'}
 json_file = open('json_file.json')
 concepts = json.load(json_file)
 
-url = "http://localhost:8080/openmrs/ws/rest/v1/concept/"
+
 
 for key in concepts.keys():
+
+	url = "http://localhost:8080/openmrs/ws/rest/v1/concept/"
 	answer_uuids = []
 	question_uuid = ""
 	question_answer_dict = concepts[key]
 	for key1 in question_answer_dict.keys():
 		if "question" in key1:
+		
 			r = requests.post(url, data=json.dumps(question_answer_dict[key1]), headers=headers, auth=('admin', 'Admin123'))
+
 			for key2 in r.json().keys():
-					if "uuid" in key2:
-						question_uuid = r.json()[key2]
+				if "uuid" in key2:
+					question_uuid = r.json()[key2]
+		
 		elif "answer" in key1 : 
 			answersList = question_answer_dict[key1]
 			for answer in answersList:
+			
 				r = requests.post(url, data=json.dumps(answer), headers=headers, auth=('admin', 'Admin123'))
+
 				for key2 in r.json().keys():
 					if "uuid" in key2:
 						answer_uuids.append(r.json()[key2])
 
-	for subKey in question_answer_dict.keys():
-		if "question" in subKey:
+	for key1 in question_answer_dict.keys():
+		if "question" in key1:
 			url = url+question_uuid
+
 			answerPost = {"answers": answer_uuids}
 			r = requests.post(url, data=json.dumps(answerPost), headers=headers, auth=('admin', 'Admin123'))
+	
 
